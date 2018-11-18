74805041b1071cb2b5ce330da26bd758d83a78fa
Sosuke Suzuki <aosukeke@gmail.com>
2018-11-18 10:47:27 +0900

    Fix layout

--- src/components/templates/Renderer.tsx
+++ src/components/templates/Renderer.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50% 1fr;
+`
 
 const Renderer = () => (
-  <Container></Container>
+  <Container>
+    <div></div>
+    <div></div>
+  </Container>
 )
 
 export default Renderer
