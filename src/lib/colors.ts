cc94f16be5c61857d311b0a77f6ee0153fd1d236
Sosuke Suzuki <aosukeke@gmail.com>
2018-11-25 05:23:36 +0900

    Add dracula colors

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -9,3 +9,18 @@ export const grey = [
   "#343a40",
   "#212529"
 ];
+
+export const dracula = {
+  background: "#282a36",
+  currentLine: "#44475a",
+  selection: "#44475a",
+  foreground: "#f8f8f2",
+  comment: "#6272a4",
+  cyan: "#8be9fd",
+  green: "#50fa7b",
+  orange: "#ffb86c",
+  pink: "#ff79c6",
+  purple: "#bd93f9",
+  red: "#ff5555",
+  yellow: "#f1fa8c"
+};
