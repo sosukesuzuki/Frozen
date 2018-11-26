1c2a66312d85d9b85032f6bb795c206f268340fa
Sosuke Suzuki <aosukeke@gmail.com>
2018-11-26 19:57:29 +0900

    Support katex

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import "normalize-css";
 import "github-markdown-css";
+import "katex/dist/katex.min.css";
 import App from "./components/App";
 import { generateFile } from "./lib/utils";
 import { MarkdownFilesStore } from "./stores";
 import { getIsFirstAccess, setIsFirstAccess } from "./lib/localStorage";
 import readmeString from "./lib/readmeString";
+
 (async () => {
   const markdownFilesStore = new MarkdownFilesStore();
   await markdownFilesStore.init();
--- src/lib/utils/markdownProcessor.ts
+++ src/lib/utils/markdownProcessor.ts
@@ -1,9 +1,13 @@
 import remark from "remark";
 import breaks from "remark-breaks";
 import emoji from "remark-emoji";
+import math from "remark-math";
+import katex from "remark-html-katex";
 import html from "remark-html";
 
 export const markdownProcessor = remark()
   .use(breaks)
   .use(emoji)
+  .use(math)
+  .use(katex)
   .use(html);
