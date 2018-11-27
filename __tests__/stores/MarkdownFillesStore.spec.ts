1edb014d8e000752932e65faa097e81dc208b4b1
Sosuke Suzuki <aosukeke@gmail.com>
2018-11-28 05:51:04 +0900

    Add MarkdownFilesStore#addFile test

--- __tests__/stores/MarkdownFillesStore.spec.ts
+++ __tests__/stores/MarkdownFillesStore.spec.ts
@@ -65,4 +65,52 @@ describe("MarkdownFilesStore", () => {
       expect(result).toBeUndefined();
     });
   });
+
+  describe("#addFile", () => {
+    it("adds a new file", async () => {
+      // Given
+      const store = new MarkdownFilesStore();
+      const dummyFile1 = {
+        title: "title",
+        content: "content",
+        id: "12345"
+      };
+      const dummyFile2 = {
+        title: "title",
+        content: "content",
+        id: "67890"
+      };
+      store.files = [dummyFile1];
+
+      // When
+      await store.addFile(dummyFile2);
+
+      // Then
+      expect(store.files).toMatchObject([dummyFile1, dummyFile2]);
+    });
+
+    it("adds a new file and set this file to current file when set setFile option", async () => {
+      // Given
+      const store = new MarkdownFilesStore();
+      const dummyFile1 = {
+        title: "title",
+        content: "content",
+        id: "12345"
+      };
+      const dummyFile2 = {
+        title: "title",
+        content: "content",
+        id: "67890"
+      };
+      store.files = [dummyFile1];
+      store.currentFileIndex = 0;
+
+      // When
+      await store.addFile(dummyFile2, { setFile: true });
+
+      // Then
+      expect(store.files).toMatchObject([dummyFile1, dummyFile2]);
+      expect(store.currentFileIndex).toBe(1);
+    });
+  });
 });
