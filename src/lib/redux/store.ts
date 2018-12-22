fa942d931651170c1b804260c07cbce13f3e0d2c
Sosuke Suzuki <aosukeke@gmail.com>
2018-12-22 21:58:55 +0900

    Remove logger middleware from production build

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -4,9 +4,15 @@ import logger from "redux-logger";
 import reducer from "./reducer";
 import rootSaga from "./saga";
 
+const env = process.env.NODE_ENV;
+
 const sagaMiddleware = createSagaMiddleWare();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
+const store =
+  env === "production"
+    ? createStore(reducer, applyMiddleware(sagaMiddleware))
+    : createStore(reducer, applyMiddleware(sagaMiddleware, logger));
+
 sagaMiddleware.run(rootSaga);
 
 export default store;
