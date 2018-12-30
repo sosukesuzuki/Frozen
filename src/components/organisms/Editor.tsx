0100a6067bc99a4789220e8f975bdace2e87494e
Sosuke Suzuki <aosukeke@gmail.com>
2018-12-30 22:47:02 +0900

    It works

--- src/components/organisms/Editor.tsx
+++ src/components/organisms/Editor.tsx
@@ -26,7 +26,9 @@ const cmOptions = {
 const TextareaContainer = styled.div`
   padding: 20px;
   background-color: ${dracula.background};
+  height: calc(100vh - 70px);
   .CodeMirror {
+    z-index: 0;
     height: 100%;
     bottom: 0;
   }
@@ -42,51 +44,54 @@ const Editor: React.FC<Props> = ({ file, updateFile }) => {
   const textarea = useRef<HTMLTextAreaElement>(null);
   const disabledHandleChange = useRef<boolean>(false);
 
+  useEffect(
+    () => {
+      if (textarea && textarea.current) {
+        // React.RefObject is broken...
+        (cm.current as CodeMirror.EditorFromTextArea) = CodeMirror.fromTextArea(
+          textarea.current,
+          cmOptions
+        );
+
+        disabledHandleChange.current = false;
+
+        cm.current!.getDoc().setValue(file.content);
+        cm.current!.on("change", onChange);
+      }
+
+      return () => {
+        if (cm && cm.current) {
+          cm.current!.off("change", onChange);
+          cm.current!.toTextArea();
+        }
+      };
+    },
+    [file.id]
+  );
+
   useEffect(() => {
     if (textarea && textarea.current) {
-      // React.RefObject is broken...
-      (cm.current as CodeMirror.EditorFromTextArea) = CodeMirror.fromTextArea(
-        textarea.current,
-        cmOptions
-      );
-
-      disabledHandleChange.current = false;
-
-      cm.current!.getDoc().setValue(file.content);
-      cm.current!.on("change", onChange);
-    }
-
-    return () => {
-      if (cm && cm.current) {
-        cm.current!.off("change", onChange);
-        cm.current!.toTextArea();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (cm && cm.current) {
       const doc = cm.current!.getDoc();
       const isValueChanged = file.content !== doc.getValue();
 
-      if (isValueChanged && !disabledHandleChange) {
+      if (isValueChanged && !disabledHandleChange.current) {
         doc.setValue(file.content);
       }
     }
   });
 
-  function onChange(cm: CodeMirror.Editor) {
+  const onChange = (cm: CodeMirror.Editor) => {
     const value = cm.getDoc().getValue();
     disabledHandleChange.current = true;
 
     updateFile(file.id, value);
 
     disabledHandleChange.current = false;
-  }
+  };
 
   return (
     <TextareaContainer>
-      <textarea ref={textarea} />
+      <textarea className="editor" ref={textarea} />
     </TextareaContainer>
   );
 };
