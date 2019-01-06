bb00d7a0a8e4398f8fcf3018a3e0f2737aaa0516
Sosuke Suzuki <aosukeke@gmail.com>
2019-01-06 22:43:34 +0900

    Fix bug

--- src/lib/redux/sagas/workspacesSaga.ts
+++ src/lib/redux/sagas/workspacesSaga.ts
@@ -36,7 +36,7 @@ function* deleteWorkspaceSaga(db: DBServiceInterface): SagaIterator {
     const { payload } = yield take(ActionTypes.DELETE_WORKSPACE);
     const { id } = payload;
     const oldWorkspaces = yield select((state: State) => state.workspaces);
-    if (oldWorkspaces.length === 1) {
+    if (oldWorkspaces.length !== 1) {
       yield call(db.deleteWorkspace, id);
       const workspaces: Workspace[] = yield call(db.getWorkspaces);
       yield put(actionCreators.setDeletedWorkspace(workspaces));
